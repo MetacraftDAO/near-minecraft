2ebae0c1c9deecda131c5cdfb44b0f18c88b79bd
Sharu <jsrshark110@gmail.com>
2022-02-13 23:24:52 -0800

    Hide api key

--- minecraft-mod/src/main/java/com/example/examplemod/AccountLoginCommand.java
+++ minecraft-mod/src/main/java/com/example/examplemod/AccountLoginCommand.java
@@ -13,10 +13,18 @@ import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 import java.net.http.HttpClient;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.lang.RuntimeException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class AccountLoginCommand {
     // Directly reference a log4j logger.
@@ -24,25 +32,49 @@ public class AccountLoginCommand {
 
     public AccountLoginCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("account").then(Commands.literal("login").executes((command) -> {
-            return VerifyNearAccount(command.getSource());
+            return LoginNearAccount(command.getSource());
         })));
     }
 
-    private int VerifyNearAccount(CommandSourceStack source) throws CommandSyntaxException {
+    private int LoginNearAccount(CommandSourceStack source) throws CommandSyntaxException {
         source.sendSuccess(new TextComponent("test test"), true);
         try {
-            HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://parseapi.back4app.com/classes/Channel"))
-                    .header("X-Parse-Application-Id", "3NYTcCWrSdGasNKlg03cQP68qBk7URgK4RCXXk45")
-                    .header("X-Parse-Master-Key", "t0Bk4ShUpoHtYP1AJPuYbbLMgUzWzuxaYvMSu1PO")
-                    .GET()
-                    .build();
+            HttpRequest.Builder builder = HttpRequest.newBuilder()
+                    .uri(new URI("https://parseapi.back4app.com/classes/Channel"));
+            HashMap<String, String> headers = GetDBHeaders();
+            headers.forEach((key, value) -> builder.header(key, value));
+            HttpRequest request = builder.GET().build();
             HttpClient client = HttpClient.newHttpClient();
-            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            LOGGER.info("Status: ", response.statusCode());
-            LOGGER.info("Response body: ", response.body());
-        } catch (URISyntaxException | IOException | InterruptedException err) {
+            HttpResponse<String> response = client.send(request,
+                    BodyHandlers.ofString());
+            LOGGER.info("Status: " + String.valueOf(response.statusCode()));
+            LOGGER.info("Response body: " + response.body());
+        } catch (URISyntaxException | IOException | InterruptedException | RuntimeException err) {
             LOGGER.info("Error:", err.toString());
         }
         return 1;
     }
+
+    private HashMap<String, String> GetDBHeaders() throws IOException {
+        HashMap<String, String> headers = new HashMap<String, String>();
+        Path homePath = Paths.get(System.getProperty("user.home"));
+
+        Path keyPath = Paths.get(homePath.toString(), ".back4app_key");
+        String key = ReadFileContent(keyPath.toString());
+        headers.put("X-Parse-Master-Key", key);
+
+        Path appIdPath = Paths.get(homePath.toString(), ".back4app_app_id");
+        String appId = ReadFileContent(appIdPath.toString());
+        headers.put("X-Parse-Application-Id", appId);
+        return headers;
+    }
+
+    private String ReadFileContent(String path) throws IOException {
+        File file = new File(path);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String content = buffer.lines().collect(Collectors.joining());
+        buffer.close();
+        return content;
+    }
+
 }
