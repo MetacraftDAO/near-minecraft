3cc8c890412f2b9827668c06fc39bc98c90198e2
Yicheng Li <ycli@alumni.stanford.edu>
2022-02-20 15:12:24 -0800

    Fix lighting in GlassPrison

--- minecraft-mod/src/main/java/com/example/examplemod/blocks/UnbreakableGlassBlock.java
+++ minecraft-mod/src/main/java/com/example/examplemod/blocks/UnbreakableGlassBlock.java
@@ -1,14 +1,23 @@
 package com.example.examplemod.blocks;
 
-import net.minecraft.world.level.block.GlassBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.AbstractGlassBlock;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 
-public class UnbreakableGlassBlock extends GlassBlock {
+public class UnbreakableGlassBlock extends AbstractGlassBlock {
     public UnbreakableGlassBlock() {
         super(Properties.of(Material.GLASS)
                 .sound(SoundType.GLASS)
                 .strength(-1.0F, 3600000.0F)
+                .noOcclusion()
                 .noDrops());
+    }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos){
+        return true;
     }
 }
